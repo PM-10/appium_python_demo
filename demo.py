8cfde88f16996d1ad7ad042ca306de9455d511e3
Gibson <show9114@gmail.com>
2023-09-26 22:11:00 +0900

    set project

---
+++ demo.py
@@ -0,0 +1,31 @@
+import unittest
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+from appium.options.android import UiAutomator2Options
+
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='Android',
+    appPackage='com.android.settings',
+    appActivity='.Settings',
+)
+
+appium_server_url = 'http://localhost:4723'
+
+
+class TestAppium(unittest.TestCase):
+    def setUp(self) -> None:
+        capability_options = UiAutomator2Options().load_capabilities(capabilities)
+        self.driver = webdriver.Remote(appium_server_url, options=capability_options)
+
+    def tearDown(self) -> None:
+        print("### FINISH")
+
+    def test_find_battery(self) -> None:
+        el = self.driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
+        el.click()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ emulator.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const shell = require('shelljs')
+
+shell.exec('emulator -avd Pixel_3a_API_31_arm64-v8a')
